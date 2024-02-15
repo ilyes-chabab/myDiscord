74690792e82b9e148100edaca8d09ee8f98f5a05
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-02-15 11:27:06 +0100

    le crud channel a été fait

--- channel.py
+++ channel.py
@@ -6,9 +6,28 @@ class Channel:
         self.db=Db('localhost','root','root','discord')
         self.db.connect()
 
-    def read(self):
-        self.db.cursor.execute('select * from channel')
-        result=self.db.cursor.fetchall()
-        print(result)
+    def createChannel(self,nameChannel):
+        queries = (" insert into channel (nom) values (%s) ")
+        params = (nameChannel,)
+        self.db.executeQuery(queries ,params)
+        print(f"{nameChannel} crée")
+     
+    def readchannel(self):
+        queries = ('select * from channel')
+        return self.db.fetch(queries)
+    
+    def updateChannel(self,id,new_name_channel):
+        queries=('UPDATE channel SET nom = %s WHERE id = %s')
+        params=(new_name_channel,id)
+        self.db.executeQuery(queries,params)
+        print(f"le channel avec l'id {id} a été modifié en {new_name_channel}")
+
+    def deleteChannel(self,id):
+        queries=('DELETE FROM channel WHERE id = %s')
+        params= (id,)
+        self.db.executeQuery(queries,params)
+        print(f"le channel avec l'{id} a été supprimé")  
+
 channel=Channel()
-channel.read()
+print(channel.readchannel())
+
